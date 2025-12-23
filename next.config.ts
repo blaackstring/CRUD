feab8f4f2f9d8164bc56b7b941190f7b8ac51a44
shahan <johndoe@email.com>
2025-12-24 01:32:28 +0530

    update image configuration to use remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["randomuser.me","plus.unsplash.com"],
+    remotePatterns:[new URL("randomuser.me"),new URL("plus.unsplash.com")],
   },
 };
 
